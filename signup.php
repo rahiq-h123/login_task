4ff3dee583668a160230da07a0125b9cf0471dc0
rahiq-h123 <77285448+rahiq-h123@users.noreply.github.com>
2022-09-18 23:25:48 +0200

    Update signup.php

--- signup.php
+++ signup.php
@@ -6,7 +6,7 @@
                 <div class="signup-content">
                     <div class="signup-form">
                         <h2 class="form-title">Sign up</h2>
-                        <form method="POST" action="signup.php" class="register-form" id="register-form">
+                        <form method="POST" action="signup-add.php" class="register-form" id="register-form">
                             <div class="form-group">
                                 <label for="name"><i class="zmdi zmdi-account material-icons-name"></i></label>
                                 <input type="text" name="name" id="name" placeholder="Your Name"/>
@@ -34,7 +34,7 @@
                     </div>
                     <div class="signup-image">
                         <figure><img src="images/signup-image.jpg" alt="sing up image"></figure>
-                        <a href="#" class="signup-image-link">I am already member</a>
+                        <a href="login.php" class="signup-image-link">I am already member</a>
                     </div>
                 </div>
             </div>
